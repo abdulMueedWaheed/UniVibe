f35725401c7c24c5e7ca3474474dff124d146bc3
Abdullah <abdullah.bscs23seecs@seecs.edu.pk>
2025-04-16 00:44:16 +0500

    Created Login Page frontend

---
+++ client/src/pages/register/Register.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './register.scss'
+
+const Register = () => {
+    return (
+        <div className='register'>
+          <div className="card">
+            <div className="left">
+              <h1>Uni Vibe.</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, cum
+                voluptas? Incidunt iusto ipsum nesciunt repellendus tenetur.
+              </p>
+              <span>Don't have an account? </span>
+              <button>Register</button>
+    
+            </div>
+            <div className="right">
+              <h1>Login</h1>
+              <form>
+                <input type="text" placeholder="Username" name="" id="" />
+                <input type="password" placeholder="Password" />
+                <button>Login</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      )
+}
+
+export default Register
