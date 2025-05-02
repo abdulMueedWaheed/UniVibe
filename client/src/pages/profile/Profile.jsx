4a3aa0562ac857eaf8ebfbb7259d4844b9b77f7a
Abdul Mueed Waheed <awaheed.bscs23seecs@seecs.edu.pk>
2025-05-02 18:55:47 +0500

    just somethings setup.

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -3,7 +3,36 @@ import './profile.scss'
 
 const Profile = () => {
   return (
-    <div className='profile'>Profile</div>
+    <div className='profile'>
+      <div className="images">
+        <img src="" alt="" className='cover'/>
+        <img src="" alt="" className='profilePic'/>
+      </div>
+
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoToneIcon fontSize="large"/>
+            </a>
+            <a href="http://facebook.com">
+              <InstagramIcon fontSize="large"/>
+            </a>
+            <a href="http://facebook.com">
+              <TwitterIcon fontSize="large"/>
+            </a>
+            <a href="http://facebook.com">
+              <LinkedInIcon fontSize="large"/>
+            </a>
+            <a href="http://facebook.com">
+              <PinterestIcon fontSize="large"/>
+            </a>
+          </div>
+          <div className="center"></div>
+          <div className="right"></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
